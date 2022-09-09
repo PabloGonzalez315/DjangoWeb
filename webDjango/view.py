f27a7cad130ba57a85f3fd3460715f2c720efc9d
Pablo <pablo.alejandro315@gmail.com>
2022-09-08 21:57:17 -0300

    Inicializacion proyecto webDjango

---
+++ webDjango/view.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def home(self,name):
+    return HttpResponse(f'hola soy la pagina Home {name}')
+
+def home3(self):
+    planilla = loader.get_template('home.html')
+    documento = planilla.render()
+    return HttpResponse(documento)
+
+def homePage(self):
+    lista = [1,2,3,4,5]
+    data = {'nombre':'pablo', 'apellido': 'gonzalez', 'lista':lista } 
+    planilla = loader.get_template('homepage.html')
+    documento = planilla.render(data)
+    return HttpResponse(documento)
